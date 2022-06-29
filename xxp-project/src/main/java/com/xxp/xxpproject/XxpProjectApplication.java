cbf3dffe8f105f64fc41f57e20dd9665928759d6
“觉罗晓“ <1029290884@qq.com>
2022-06-29 15:11:01 +0800

    springcould代码初次提交

---
+++ xxp-project/src/main/java/com/xxp/xxpproject/XxpProjectApplication.java
@@ -0,0 +1,13 @@
+package com.xxp.xxpproject;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class XxpProjectApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(XxpProjectApplication.class, args);
+    }
+
+}
---
+++ xxp-project/src/test/java/com/xxp/xxpproject/XxpProjectApplicationTests.java
@@ -0,0 +1,13 @@
+package com.xxp.xxpproject;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class XxpProjectApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
